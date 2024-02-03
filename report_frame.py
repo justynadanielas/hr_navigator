665afdd2dd2820a2145f2384814b9c21a67185a4
justynadanielas <justiyandere@gmail.com>
2024-02-03 15:04:37 +0100

    add confirmation and rejection to report frame

--- report_frame.py
+++ report_frame.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from tkinter import messagebox
+
+from raport import Raport
 
 
 class ReportFrame(tk.Frame):
@@ -11,11 +14,16 @@ class ReportFrame(tk.Frame):
         self.pack()
         self.root.opinion_frame.pack_forget()
 
-    def set_confirmation(self):
-        pass
+    def confirm(self, report: Raport):
+        report.is_confirmed = True
+        messagebox.showinfo("Raport", "Raport potwierdzony")
+        self.refresh_report_frame()
 
-    def set_rejection(self):
-        pass
+    def reject(self, report: Raport):
+        report.is_rejected = True
+        messagebox.showinfo("Raport", "Raport odrzucony")
+        self.refresh_report_frame()
+
 
     def refresh_report_frame(self):
         for widget in self.winfo_children():
@@ -40,7 +48,14 @@ class ReportFrame(tk.Frame):
                 label_report_body.grid(row=1, column=0, padx=20)
                 label_report_date = tk.Label(self, text=report.date)
                 label_report_date.grid(row=1, column=1, padx=20)
-                button_zatwierdz = tk.Button(self, text="Zatwierdź", command=self.set_confirmation)
-                button_zatwierdz.grid(row=1, column=2)
-                button_odrzuc = tk.Button(self, text="Odrzuć", command=self.set_rejection)
-                button_odrzuc.grid(row=1, column=3)
+                if not (report.is_confirmed or report.is_rejected):
+                    button_zatwierdz = tk.Button(self, text="Zatwierdź", command=lambda r=report: self.confirm(r))
+                    button_zatwierdz.grid(row=1, column=2)
+                    button_odrzuc = tk.Button(self, text="Odrzuć", command=lambda r=report: self.reject(r))
+                    button_odrzuc.grid(row=1, column=3)
+                elif report.is_confirmed:
+                    label_confirmed = tk.Label(self, text="Zatwierdzony")
+                    label_confirmed.grid(row=1, column=2)
+                elif report.is_rejected:
+                    label_rejected = tk.Label(self, text="Odrzucony")
+                    label_rejected.grid(row=1, column=2)
